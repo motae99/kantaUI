bc330b5a4fdd65a7f148ad28c6c6e170b9bf408e
motae99 <motae99@gmail.com>
2021-01-26 18:51:23 +0530

    With Assets Folder and Lottie

--- App.js
+++ App.js
@@ -4,20 +4,24 @@ import {
   StatusBar,
   Animated,
   Text,
-  Image,
   View,
   StyleSheet,
   Dimensions,
   Platform,
   SafeAreaView,
 } from 'react-native';
+// eslint-disable-next-line no-unused-vars
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import LottieView from 'lottie-react-native';
+import codePush from 'react-native-code-push';
+
+const codePushOptions = {
+  updateDialog: true,
+  checkFrequency: codePush.CheckFrequency.ON_APP_START,
+  installMode: codePush.InstallMode.IMMEDIATE,
+};
+
 const {width, height} = Dimensions.get('window');
-
-// https://www.flaticon.com/packs/retro-wave
-// inspiration: https://dribbble.com/shots/11164698-Onboarding-screens-animation
-// https://twitter.com/mironcatalin/status/1321180191935373312
-
 const bgs = ['#28abb9', '#6a097d', '#01c5c4', '#B98EFF'];
 const DATA = [
   {
@@ -25,27 +29,27 @@ const DATA = [
     title: 'Multi-lateral intermediate moratorium',
     description:
       "I'll back up the multi-byte XSS matrix, that should feed the SCSI application!",
-    image: 'https://image.flaticon.com/icons/png/256/3571/3571572.png',
+    image: require('./assets/Lottie/first.json'),
   },
   {
     key: '3571747',
     title: 'Automated radical data-warehouse',
     description:
       'Use the optical SAS system, then you can navigate the auxiliary alarm!',
-    image: 'https://image.flaticon.com/icons/png/256/3571/3571747.png',
+    image: require('./assets/Lottie/second.json'),
   },
   {
     key: '3571680',
     title: 'Inverse attitude-oriented system engine',
     description:
       'The ADP array is down, compress the online sensor so we can input the HTTP panel!',
-    image: 'https://image.flaticon.com/icons/png/256/3571/3571680.png',
+    image: require('./assets/Lottie/first.json'),
   },
   {
     key: '3571603',
     title: 'Monitored global data-warehouse',
     description: 'We need to program the open-source IB interface!',
-    image: 'https://image.flaticon.com/icons/png/256/3571/3571603.png',
+    image: require('./assets/Lottie/second.json'),
   },
 ];
 
@@ -138,11 +142,11 @@ const Square = ({scrollx}) => {
   );
 };
 
-export default function App() {
+const App = () => {
   const scrollx = React.useRef(new Animated.Value(0)).current;
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar translucent />
+      <StatusBar translucent backgroundColor="transparent" />
       <Backdrop scrollx={scrollx} />
       <Square scrollx={scrollx} />
       <Animated.FlatList
@@ -161,13 +165,19 @@ export default function App() {
           return (
             <View style={{width, alignItems: 'center', padding: 20}}>
               <View style={{flex: 0.7, justifyContent: 'center'}}>
-                <Image
+                {/* <Image
                   source={{uri: item.image}}
                   style={{
                     width: width / 2,
                     height: height / 4,
                     resizeMode: 'contain',
                   }}
+                /> */}
+                <LottieView
+                  style={{width: width / 2, height: height / 3}}
+                  source={item.image}
+                  autoPlay
+                  loop
                 />
               </View>
               <View style={{flex: 0.3}}>
@@ -191,7 +201,7 @@ export default function App() {
       <Indicator scrollx={scrollx} />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -208,3 +218,5 @@ const styles = StyleSheet.create({
     // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
 });
+
+export default codePush(codePushOptions)(App);
