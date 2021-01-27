48d815071d4054a6cf78e98f55ee8204404f77a6
motae99 <motae99@gmail.com>
2021-01-27 08:41:28 +0530

    Firebase App Auth and firestore Added

--- App.js
+++ App.js
@@ -1,335 +1,342 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-
+/* eslint-disable react-native/no-inline-styles */
+import * as React from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
+  StatusBar,
   Text,
-  TextInput,
-  TouchableOpacity,
   View,
+  StyleSheet,
+  FlatList,
+  Image,
+  Dimensions,
+  Animated,
+  TouchableOpacity,
   Platform,
-  Alert,
 } from 'react-native';
+import codePush from 'react-native-code-push';
+// import {LinearGradient} from 'react-native-linear-gradient';
 
-import Spinner from 'react-native-loading-spinner-overlay';
-import Form from 'react-native-form';
-import CountryPicker from 'react-native-country-picker-modal';
+const codePushOptions = {
+  updateDialog: true,
+  checkFrequency: codePush.CheckFrequency.ON_APP_START,
+  installMode: codePush.InstallMode.IMMEDIATE,
+};
 
-const api = new Frisbee({
-  baseURI: 'http://localhost:3000',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
+const {width, height} = Dimensions.get('window');
+
+const SPACING = 10;
+const ITEM_SIZE = Platform.OS === 'ios' ? width * 0.72 : width * 0.82;
+const EMPTY_ITEM_SIZE = (width - ITEM_SIZE) / 2;
+const BACKDROP_HEIGHT = height * 0.65;
+
+const Loading = () => (
+  <View style={styles.loadingContainer}>
+    <Text style={styles.paragraph}>Loading...</Text>
+  </View>
+);
+
+const mymovies = [
+  {
+    key: '123',
+    title: 'BEAUTY',
+    poster: require('./assets/img/beauty.jpeg'),
+    backdrop: require('./assets/img/beauty.jpeg'),
+    description:
+      ' some text some text some text some text some text some text some text some text some text',
+    releaseDate: '12',
+  },
+  {
+    key: '234',
+    title: 'HOTELS',
+    poster: require('./assets/img/hotels.jpeg'),
+    backdrop: require('./assets/img/hotels.jpeg'),
+    description:
+      ' some text some text some text some text some text some text some text some text some text',
+    releaseDate: '12',
+  },
+  {
+    key: '345',
+    title: 'MAKEUP ARTISTS',
+    poster: require('./assets/img/makeup.jpeg'),
+    backdrop: require('./assets/img/makeup.jpeg'),
+    description:
+      ' some text some text some text some text some text some text some text some text some text',
+    releaseDate: '12',
+  },
+  {
+    key: '456',
+    title: 'PHOTOGRAPHY',
+    poster: require('./assets/img/photography.jpeg'),
+    backdrop: require('./assets/img/photography.jpeg'),
+    description:
+      ' some text some text some text some text some text some text some text some text some text',
+    releaseDate: '12',
+  },
+  {
+    key: '567',
+    title: 'EVENTS',
+    poster: require('./assets/img/events.jpeg'),
+    backdrop: require('./assets/img/events.jpeg'),
+    description:
+      ' some text some text some text some text some text some text some text some text some text',
+    releaseDate: '12',
+  },
+];
+
+const Backdrop = ({movies, scrollX}) => {
+  return (
+    <View style={{height: BACKDROP_HEIGHT, width, position: 'absolute'}}>
+      <FlatList
+        data={movies}
+        keyExtractor={(item) => item.key + '-backdrop'}
+        removeClippedSubviews={false}
+        contentContainerStyle={{width, height: BACKDROP_HEIGHT}}
+        renderItem={({item, index}) => {
+          if (!item.backdrop) {
+            return null;
+          }
+          // const translateX = scrollX.interpolate({
+          //   inputRange: [(index - 2) * ITEM_SIZE, (index - 1) * ITEM_SIZE],
+          //   outputRange: [0, width],
+          //   // extrapolate:'clamp'
+          // });
+          const opacity = scrollX.interpolate({
+            inputRange: [(index - 2) * ITEM_SIZE, (index - 1) * ITEM_SIZE],
+            outputRange: [0, 1],
+            // extrapolate:'clamp'
+          });
+          // let back = require(item.backdrop)
+          return (
+            <View>
+              <Animated.View
+                removeClippedSubviews={false}
+                style={{
+                  position: 'absolute',
+                  // width: translateX,
+                  opacity,
+                  height,
+                  // overflow: 'hidden',
+                }}>
+                <Image
+                  source={item.backdrop}
+                  style={{
+                    width,
+                    height: BACKDROP_HEIGHT,
+                    position: 'absolute',
+                  }}
+                />
+                <View
+                  style={{
+                    width: 255,
+                    position: 'absolute',
+                    top: 130,
+                    left: 60,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text
+                    style={{fontSize: 45, color: '#fff', textAlign: 'center'}}>
+                    {item.title}
+                  </Text>
+                </View>
+              </Animated.View>
+            </View>
+          );
+        }}
+      />
+      {/* <LinearGradient
+        colors={['rgba(0, 0, 0, 0)', 'white']}
+        style={{
+          height: BACKDROP_HEIGHT / 1.5,
+          width,
+          position: 'absolute',
+          bottom: 0,
+        }}
+      /> */}
+      <View style={{position: 'absolute', top: 50, left: 20}}>
+        <Text style={{fontSize: 25, color: '#fff', textAlign: 'center'}}>
+          KANTA BOOK
+        </Text>
+      </View>
+
+      <View
+        style={{
+          position: 'absolute',
+          top: 50,
+          right: 20,
+          height: 40,
+          width: 40,
+          borderRadius: 20,
+          backgroundColor: 'white',
+        }}>
+        <Image
+          source={{
+            uri:
+              'https://www.flaticon.com/svg/vstatic/svg/4061/4061283.svg?token=exp=1610923650~hmac=1dd0068cddddc21a29b99511cf3ee25c',
+          }}
+          style={{width: width / 2, height: height / 4, resizeMode: 'contain'}}
+        />
+      </View>
+    </View>
+  );
+};
+
+const App = () => {
+  const [movies, setMovies] = React.useState([]);
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const movies = mymovies; //await getMovies();
+      // Add empty items to create fake space
+      // [empty_item, ...movies, empty_item]
+      setMovies([{key: 'empty-left'}, ...mymovies, {key: 'empty-right'}]);
+    };
+
+    if (movies.length === 0) {
+      fetchData(movies);
+    }
+  }, [movies]);
+
+  if (movies.length === 0) {
+    return <Loading />;
   }
-});
 
-const MAX_LENGTH_CODE = 6;
-const MAX_LENGTH_NUMBER = 20;
+  return (
+    <View style={styles.container}>
+      <Backdrop movies={movies} scrollX={scrollX} />
+      <StatusBar translucent backgroundColor="transparent" />
+      <Animated.FlatList
+        showsHorizontalScrollIndicator={false}
+        data={movies}
+        keyExtractor={(item) => item.key}
+        horizontal
+        bounces={false}
+        decelerationRate={Platform.OS === 'ios' ? 0 : 0.98}
+        renderToHardwareTextureAndroid
+        contentContainerStyle={{alignItems: 'center'}}
+        snapToInterval={ITEM_SIZE}
+        snapToAlignment="start"
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {x: scrollX}}}],
+          {useNativeDriver: false},
+        )}
+        scrollEventThrottle={16}
+        renderItem={({item, index}) => {
+          if (!item.poster) {
+            return <View style={{width: EMPTY_ITEM_SIZE}} />;
+          }
 
-// if you want to customize the country picker
-const countryPickerCustomStyles = {};
+          const inputRange = [
+            (index - 2) * ITEM_SIZE,
+            (index - 1) * ITEM_SIZE,
+            index * ITEM_SIZE,
+          ];
 
-// your brand's theme primary color
-const brandColor = '#744BAC';
+          const translateY = scrollX.interpolate({
+            inputRange,
+            outputRange: [140, 100, 140],
+            extrapolate: 'clamp',
+          });
+
+          return (
+            <View style={{width: ITEM_SIZE}}>
+              <Animated.View
+                style={{
+                  marginHorizontal: SPACING,
+                  padding: SPACING * 2,
+                  alignItems: 'center',
+                  transform: [{translateY}],
+                  backgroundColor: 'white',
+                  borderRadius: 34,
+                }}>
+                <Image source={item.poster} style={styles.posterImage} />
+                {/* <Text style={{ fontSize: 24 }} numberOfLines={1}>
+                  {item.title}
+                </Text> */}
+                {/* <Rating rating={item.rating} />
+                <Genres genres={item.genres} /> */}
+                <Text
+                  style={{fontSize: 13, textAlign: 'center', color: '#3A4154'}}
+                  numberOfLines={3}>
+                  {item.description}
+                </Text>
+                <TouchableOpacity
+                  style={{
+                    margin: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: 60,
+                    width: '95%',
+                    backgroundColor: '#2C3449',
+                    borderRadius: 10,
+                  }}
+                  onPress={() => {}}>
+                  <Text
+                    style={{color: '#fff', fontSize: 28, textAlign: 'center'}}>
+                    See All
+                  </Text>
+                </TouchableOpacity>
+              </Animated.View>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  countryPicker: {
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  container: {
-    flex: 1
-  },
-  header: {
-    textAlign: 'center',
-    marginTop: 60,
-    fontSize: 22,
-    margin: 20,
-    color: '#4A4A4A',
-  },
-  form: {
-    margin: 20
-  },
-  textInput: {
-    padding: 0,
-    margin: 0,
+  loadingContainer: {
     flex: 1,
-    fontSize: 20,
-    color: brandColor
-  },
-  button: {
-    marginTop: 20,
-    height: 50,
-    backgroundColor: brandColor,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 5,
   },
-  buttonText: {
-    color: '#fff',
-    fontFamily: 'Helvetica',
-    fontSize: 16,
-    fontWeight: 'bold'
+  container: {
+    flex: 1,
   },
-  wrongNumberText: {
-    margin: 10,
-    fontSize: 14,
-    textAlign: 'center'
-  },
-  disclaimerText: {
-    marginTop: 30,
-    fontSize: 12,
-    color: 'grey'
-  },
-  callingCodeView: {
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  callingCodeText: {
-    fontSize: 20,
-    color: brandColor,
-    fontFamily: 'Helvetica',
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
     fontWeight: 'bold',
-    paddingRight: 10
-  }
+    textAlign: 'center',
+  },
+  posterImage: {
+    width: '98%',
+    height: ITEM_SIZE,
+    resizeMode: 'cover',
+    borderRadius: 24,
+    margin: 0,
+    marginBottom: 10,
+  },
 });
+export default codePush(codePushOptions)(App);
 
-export default class example extends Component {
+// import React from 'react';
+// import {View} from 'react-native';
+// import LottieView from 'lottie-react-native';
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      enterCode: false,
-      spinner: false,
-      country: {
-        cca2: 'US',
-        callingCode: '1'
-      }
-    };
-  }
+// export default class BasicExample extends React.Component {
+//   componentDidMount() {
+//     this.animation.play();
+//     // Or set a specific startFrame and endFrame with:
+//     this.animation.play(30, 1000);
+//   }
 
-  _getCode = () => {
-
-    this.setState({ spinner: true });
-
-    setTimeout(async () => {
-
-      try {
-
-        const res = await api.post('/v1/verifications', {
-          body: {
-            ...this.refs.form.getValues(),
-            ...this.state.country
-          }
-        });
-
-        if (res.err) throw res.err;
-
-        this.setState({
-          spinner: false,
-          enterCode: true,
-          verification: res.body
-        });
-        this.refs.form.refs.textInput.setNativeProps({ text: '' });
-
-        setTimeout(() => {
-          Alert.alert('Sent!', "We've sent you a verification code", [{
-            text: 'OK',
-            onPress: () => this.refs.form.refs.textInput.focus()
-          }]);
-        }, 100);
-
-      } catch (err) {
-        // <https://github.com/niftylettuce/react-native-loading-spinner-overlay/issues/30#issuecomment-276845098>
-        this.setState({ spinner: false });
-        setTimeout(() => {
-          Alert.alert('Oops!', err.message);
-        }, 100);
-      }
-
-    }, 100);
-
-  }
-
-  _verifyCode = () => {
-
-    this.setState({ spinner: true });
-
-    setTimeout(async () => {
-
-      try {
-
-        const res = await api.put('/v1/verifications', {
-          body: {
-            ...this.refs.form.getValues(),
-            ...this.state.country
-          }
-        });
-
-        if (res.err) throw res.err;
-
-        this.refs.form.refs.textInput.blur();
-        // <https://github.com/niftylettuce/react-native-loading-spinner-overlay/issues/30#issuecomment-276845098>
-        this.setState({ spinner: false });
-        setTimeout(() => {
-          Alert.alert('Success!', 'You have successfully verified your phone number');
-        }, 100);
-
-      } catch (err) {
-        // <https://github.com/niftylettuce/react-native-loading-spinner-overlay/issues/30#issuecomment-276845098>
-        this.setState({ spinner: false });
-        setTimeout(() => {
-          Alert.alert('Oops!', err.message);
-        }, 100);
-      }
-
-    }, 100);
-
-  }
-
-  _onChangeText = (val) => {
-    if (!this.state.enterCode) return;
-    if (val.length === MAX_LENGTH_CODE)
-    this._verifyCode();
-  }
-
-  _tryAgain = () => {
-    this.refs.form.refs.textInput.setNativeProps({ text: '' })
-    this.refs.form.refs.textInput.focus();
-    this.setState({ enterCode: false });
-  }
-
-  _getSubmitAction = () => {
-    this.state.enterCode ? this._verifyCode() : this._getCode();
-  }
-
-  _changeCountry = (country) => {
-    this.setState({ country });
-    this.refs.form.refs.textInput.focus();
-  }
-
-  _renderFooter = () => {
-
-    if (this.state.enterCode)
-      return (
-        <View>
-          <Text style={styles.wrongNumberText} onPress={this._tryAgain}>
-            Enter the wrong number or need a new code?
-          </Text>
-        </View>
-      );
-
-    return (
-      <View>
-        <Text style={styles.disclaimerText}>By tapping "Send confirmation code" above, we will send you an SMS to confirm your phone number. Message &amp; data rates may apply.</Text>
-      </View>
-    );
-
-  }
-
-  _renderCountryPicker = () => {
-
-    if (this.state.enterCode)
-      return (
-        <View />
-      );
-
-    return (
-      <CountryPicker
-        ref={'countryPicker'}
-        closeable
-        style={styles.countryPicker}
-        onChange={this._changeCountry}
-        cca2={this.state.country.cca2}
-        styles={countryPickerCustomStyles}
-        translation='eng'/>
-    );
-
-  }
-
-  _renderCallingCode = () => {
-
-    if (this.state.enterCode)
-      return (
-        <View />
-      );
-
-    return (
-      <View style={styles.callingCodeView}>
-        <Text style={styles.callingCodeText}>+{this.state.country.callingCode}</Text>
-      </View>
-    );
-
-  }
-
-  render() {
-
-    let headerText = `What's your ${this.state.enterCode ? 'verification code' : 'phone number'}?`
-    let buttonText = this.state.enterCode ? 'Verify confirmation code' : 'Send confirmation code';
-    let textStyle = this.state.enterCode ? {
-      height: 50,
-      textAlign: 'center',
-      fontSize: 40,
-      fontWeight: 'bold',
-      fontFamily: 'Courier'
-    } : {};
-
-    return (
-
-      <View style={styles.container}>
-
-        <Text style={styles.header}>{headerText}</Text>
-
-        <Form ref={'form'} style={styles.form}>
-
-          <View style={{ flexDirection: 'row' }}>
-
-            {this._renderCountryPicker()}
-            {this._renderCallingCode()}
-
-            <TextInput
-              ref={'textInput'}
-              name={this.state.enterCode ? 'code' : 'phoneNumber' }
-              type={'TextInput'}
-              underlineColorAndroid={'transparent'}
-              autoCapitalize={'none'}
-              autoCorrect={false}
-              onChangeText={this._onChangeText}
-              placeholder={this.state.enterCode ? '_ _ _ _ _ _' : 'Phone Number'}
-              keyboardType={Platform.OS === 'ios' ? 'number-pad' : 'numeric'}
-              style={[ styles.textInput, textStyle ]}
-              returnKeyType='go'
-              autoFocus
-              placeholderTextColor={brandColor}
-              selectionColor={brandColor}
-              maxLength={this.state.enterCode ? 6 : 20}
-              onSubmitEditing={this._getSubmitAction} />
-
-          </View>
-
-          <TouchableOpacity style={styles.button} onPress={this._getSubmitAction}>
-            <Text style={styles.buttonText}>{ buttonText }</Text>
-          </TouchableOpacity>
-
-          {this._renderFooter()}
-
-        </Form>
-
-        <Spinner
-          visible={this.state.spinner}
-          textContent={'One moment...'}
-          textStyle={{ color: '#fff' }} />
-
-      </View>
-
-    );
-  }
-}
-
-AppRegistry.registerComponent('example', () => example);
+//   render() {
+//     return (
+//       <View
+//         style={{
+//           flex: 1,
+//           justifyContent: 'center',
+//           alignItems: 'center',
+//         }}>
+//         <LottieView
+//           style={{height: 150, width: 150}}
+//           ref={(animation) => {
+//             this.animation = animation;
+//           }}
+//           source={require('./assets/Lottie/first.json')}
+//         />
+//       </View>
+//     );
+//   }
+// }
