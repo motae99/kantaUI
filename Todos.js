dd35e09327691653c4cf1d1b9ba46cc8dbc7cd8b
motae99 <motae99@gmail.com>
2021-04-04 14:04:34 +0530

    reverting back

--- Todos.js
+++ Todos.js
@@ -1,2 +1,7 @@
 // Requires Build
 // react-native-android-sms-listener
+
+// "@react-native-firebase/functions": "^11.2.0",
+    // "@react-native-firebase/in-app-messaging": "^11.2.0",
+    // "@react-native-firebase/messaging": "^11.2.0",
+    // "@react-native-firebase/dynamic-links": "^11.0.0",
